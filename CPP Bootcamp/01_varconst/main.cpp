f74c70d250219556926ca0c36b1eb83b921bbb11
anshthakur0999 <80532812+anshthakur0999@users.noreply.github.com>
2025-03-04 13:53:38 +0000

    var and const

---
+++ CPP Bootcamp/01_varconst/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int score;
+    score = 110;
+
+    const int uid = 232323;
+
+    int balance = 599;
+    balance = 1999;
+    // cout<<"Enter Score Value:"<<endl;
+    // cin>>score;
+    // cout<<"Score value is:"<<score<<endl;
+
+
+    return 0;
+}
