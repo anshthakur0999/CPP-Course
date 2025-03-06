7f4f205d6b13c6f0b35abe8664337d47266e6377
anshthakur0999 <80532812+anshthakur0999@users.noreply.github.com>
2025-03-06 13:32:22 +0000

    modifiers

---
+++ CPP Bootcamp/02_Primitives/data.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int teaLeaves = 50;
+    float waterTemparature = 85.5;
+    double priceOfTea = 299.9;
+    char quality = 'A';
+    bool isTeaReady = false;
+
+
+    // bool isTeaReady = 0;     //   0 equivalent false
+    // bool isTeaReady = 1;     // 1 equivalent true
+    // bool isTeaReady = 5;     // any other integer is equivalent true
+    // bool isTeaReady = 'A'   // Char is equivalent true 
+
+    
+
+
+
+
+    cout << isTeaReady << endl;
+
+    return 0;
+}
---
+++ CPP Bootcamp/02_Primitives/modifiers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // unsigned int smallTeaPack = 100;
+    unsigned int smallTeaPack = -100;     //Garbage value
+    // cout << smallTeaPack <<endl;
+
+    long largeTeaBag = 100000000000;
+    cout << largeTeaBag <<endl;
+
+
+
+    return 0;
+    
+}
